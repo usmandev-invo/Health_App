08029a258ce358bfe78565b5b508478c09ec4410
usmandev-invo <usman.dev01@invomail.io>
2024-08-27 16:42:27 +0500

    Update form-validation.ts

--- mobile/src/components/form/validations/form-validation.ts
+++ mobile/src/components/form/validations/form-validation.ts
@@ -1,11 +1,11 @@
 import * as Yup from "yup";
 // Define a validation schema using Yup
 export const validationSchema = Yup.object().shape({
-    date: Yup.date().required("Date is required"),
-    hoursOfSleep: Yup.number()
-      .required("Hours of sleep are required")
-      .min(0, "Cannot be negative"),
-    steps: Yup.number()
-      .required("Steps are required")
-      .min(0, "Cannot be negative"),
-  });
+  date: Yup.date().required("Date is required"),
+  hoursOfSleep: Yup.number()
+    .required("Hours of sleep are required")
+    .min(1, "Hours of sleep must be greater than 0"),
+  steps: Yup.number()
+    .required("Steps are required")
+    .min(1, "Steps must be greater than 0"),
+});
